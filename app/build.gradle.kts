c76321c1770c448f42536e8e430978752634a397
Yunian Edwi Risnawa <yunianedwi01@gmail.com>
2024-05-22 18:05:45 +0700

    Add dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-parcelize")
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.ktlint)
 }
 
 android {
@@ -33,6 +36,43 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+    flavorDimensions += "env"
+    productFlavors {
+        create("production") {
+            buildConfigField(
+                type = "String",
+                name = "BASE_URL",
+                value = "\"http://airline.azkazk11.my.id/api/v1/\"",
+            )
+            buildConfigField(
+                type = "String",
+                name = "KEY",
+                value = "\"..\"",
+            )
+        }
+        create("integration") {
+            buildConfigField(
+                type = "String",
+                name = "BASE_URL",
+                value = "\"http://airline.azkazk11.my.id/api/v1/\"",
+            )
+            buildConfigField(
+                type = "String",
+                name = "KEY",
+                value = "\"..\"",
+            )
+        }
+    }
+    packaging {
+        resources{
+            excludes += "META-INF/LICENSE.md"
+            excludes += "META-INF/LICENSE-notice.md"
+        }
+    }
 }
 
 dependencies {
@@ -45,4 +85,47 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //coil image loader
+    implementation(libs.coil)
+
+    //lifecycle
+    implementation(libs.lifecycle.livedata)
+    implementation(libs.lifecycle.runtime)
+    implementation(libs.lifecycle.viewmodel)
+
+    //fragment
+    implementation(libs.fragment.ktx)
+
+    //navigation
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    //room database
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+
+    //coroutine
+    implementation(libs.coroutine.core)
+    implementation(libs.coroutine.android)
+
+    //networking
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.interceptor)
+    implementation(libs.paging)
+
+    //shimmer
+    implementation(libs.shimmer)
+
+    //koin
+    implementation(libs.koin.android)
+
+    //testing
+    androidTestImplementation(libs.mockk.android)
+    testImplementation(libs.mockk.agent)
+    testImplementation(libs.coroutine.test)
+    testImplementation(libs.turbine)
+    testImplementation(libs.core.testing)
 }
